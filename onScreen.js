4d9f5b1744d624121207c3c7212fe5f1c0e1b612
vickera <vickera@yahoo.com>
2016-07-12 17:34:48 -0400

    Create onScreen.js

---
+++ onScreen.js
@@ -0,0 +1,18 @@
+	$.fn.extend({
+    	    onScreen: function(location){
+    	    	var 	scrollTop = $(window).scrollTop(),
+        		offset = $(this).offset().top,
+        		distance = (elementOffset - scrollTop);
+        		
+        	  switch(location){
+          		case 'bottom': location = 1; break;
+          		case 'top': location = window.innerHeight; break;
+          		default: location = 2;
+        	  }
+        	
+          	if(distance <= (window.innerHeight / location )) 
+          		return true;
+          	else 
+          		return false;
+    	    }
+	});
